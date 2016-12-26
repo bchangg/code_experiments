ec8daeaff8844b9119d456d065fa3d601bceb51f
Brian Chang <bchangg@users.noreply.github.com>
2016-12-26 21:53:40 +0800

    Added plus_minus, added drawing_book

--- hackerrank_challenges/algorithms/plus_minus.c
+++ hackerrank_challenges/algorithms/plus_minus.c
@@ -7,11 +7,27 @@
 #include <stdbool.h>
 
 int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int arr_i = 0; arr_i < n; arr_i++){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    for(int arr_i = 0; arr_i < size; arr_i++){
        scanf("%d",&arr[arr_i]);
     }
+
+    float pos = 0, neg = 0, zero = 0;
+    float posFrac, negFrac, zeroFrac;
+    for(int i = 0; i < size; i++) {
+        if(arr[i] < 0) {
+            neg++;
+        } else if(arr[i] > 0) {
+            pos++;
+        } else {
+            zero++;
+        }
+    }
+    posFrac = pos/size;
+    negFrac = neg/size;
+    zeroFrac = zero/size;
+    printf("%6f\n%6f\n%6f", posFrac, negFrac, zeroFrac);
     return 0;
 }
---
+++ hackerrank_challenges/week_of_code_27/drawing_book.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int p;
+    scanf("%d",&p);
+    // your code goes here
+    return 0;
+}
